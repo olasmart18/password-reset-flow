d52fcd5ae97a04c942c04b2d37ed1fad0f2f8b82
olasmartTech <eweleyeolamide10@gmail.com>
2023-06-26 06:27:06 -0700

    password encrypted

--- controllers/userCoontroller.js
+++ controllers/userCoontroller.js
@@ -3,7 +3,7 @@ import joi from 'joi';
 import bcrypt from 'bcrypt';
 // import jwt from 'jsonwewbtoken';
 
-// cretae new user
+// create new user
 export const register = async (req, res) => {
     try {
         // validate user data
@@ -13,7 +13,15 @@ export const register = async (req, res) => {
                 message: error.details[0].message
             })
         } else {
-            const user = new User(req.body)
+            const password = req.body.password;
+            const saltRound = bcrypt.genSaltSync(10);
+            const hash = bcrypt.hashSync(password, saltRound);
+            // create new user
+            const user = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: hash
+            });
             await user.save();
             res.status(200).json({
                 message: 'user created',
@@ -22,30 +30,30 @@ export const register = async (req, res) => {
         }
     } catch (err) {
         res.status(500).json({
-            message: 'somrthing went wrong'
+            message: 'something went wrong'
         });
     }
 }
 
 // login user
 export const login = async (req, res) => {
-    /**
-     * login new user
-     * create and sign token
-     * cookies and session
-     */
     try {
         // validate information provided
         const schema = joi.object({
             email: joi.string().email().required(),
             password: joi.string().required() });
         const { error } = schema.validate(req.body);
-        if (error) return res.status(400).json({ message: error.details[0].message });
+        if (error) return res.status(400).json({
+            message: error.details[0].message });
+
         const { email, password } = req.body;
-        const existUser = await User.findOne({ email: email });
-        if (!existUser) return res.json({ message: 'email is not registered' });
+        const existUser = await User.findOne({ email: email }); // find existing user
+        if (!existUser) return res.json({
+            message: 'email is not registered' });
         // compare user pasasword
-        if (password !== existUser.password) return res.json({messgae: 'email or password not correct'});
+        const correctPwd = bcrypt.compareSync(password, existUser.password);
+        if (!correctPwd) return res.json({
+            messgae: 'email or password not correct' });
         res.status(200).json({
                 message: 'login successful'
             })
